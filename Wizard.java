ba547d0f3060458ec39e5661713fbd91f384b9ab
vaitheeswarivenkatesan <56390406+vaitheeswarivenkatesan@users.noreply.github.com>
2021-01-12 17:35:14 +0530

    Create Wizard.java

---
+++ Wizard.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Wizard{
+    public static void main(String[] args){
+        Scanner kbd=new Scanner(System.in);
+        int n=kbd.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i+" Abracadabra");
+        }
+    }
+}
